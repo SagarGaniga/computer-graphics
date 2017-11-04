54406ef5e20f8de1fa7083462269e0be18052181
SagarGaniga <sagarganiga468@gmail.com>
2017-11-04 10:07:51 +0530

    Add DDA

---
+++ DDA/DDA.cpp
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<graphics.h>
+
+//Function for finding absolute value
+int abs (int n)
+{
+    return ( (n>0) ? n : ( n * (-1)));
+}
+
+//DDA Function for line generation
+void DDA(int X0, int Y0, int X1, int Y1)
+{
+    // calculate dx & dy
+    int dx = X1 - X0;
+    int dy = Y1 - Y0;
+
+    // calculate steps required for generating pixels
+    int steps = abs(dx) > abs(dy) ? abs(dx) : abs(dy);
+
+    // calculate increment in x & y for each steps
+    float Xinc = dx / (float) steps;
+    float Yinc = dy / (float) steps;
+
+    // Put pixel for each step
+    float X = X0;
+    float Y = Y0;
+    for (int i = 0; i <= steps; i++)
+    {
+        putpixel (X,Y,GREEN);  // put pixel at (X,Y)
+        X += Xinc;           // increment in x at each step
+        Y += Yinc;           // increment in y at each step
+        delay(100);          // for visualization of line-
+                             // generation step by step
+    }
+}
+
+// Driver program
+int main()
+{
+    int gd = DETECT, gm;
+
+    // Initialize graphics function
+    initgraph (&gd, &gm, "");
+
+    int X0 = 2, Y0 = 2, X1 = 140, Y1 = 160;
+    DDA(2, 2, 140, 160);
+    getch();
+    return 0;
+}
---
+++ DDA/README.md
@@ -0,0 +1,23 @@
+# DDA Algotrithm
+
+1.  calculate dx , dy
+2.  dx = X1 - X0;
+    dy = Y1 - Y0;
+
+3.  Depending upon absolute value of dx & dy choose number of steps to put pixel as
+    steps = abs(dx) > abs(dy) ? abs(dx) : abs(dy)
+
+4.  calculate increment in x & y for each steps
+
+5.  Xinc = dx / (float) steps;
+    Yinc = dy / (float) steps;
+
+6.  Put pixel for each step
+    X = X0;
+    Y = Y0;
+    for (int i = 0; i <= steps; i++)
+    {
+        putpixel (X,Y,WHITE);
+        X += Xinc;
+        Y += Yinc;
+    }
