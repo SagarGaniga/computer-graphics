20cd07ba0cc06198c55943e8ac92fd243d5fa0bc
SagarGaniga <2015sagar.ganiga@ves.ac.in>
2017-11-07 22:05:04 +0530

    Add SH

---
+++ Sutherland Hodgeman/README.md
@@ -0,0 +1,10 @@
+# Sutherland Hodgeman
+
+## Steps
+1. Draw the boundary
+2. Draw the shape
+3. Call all 4 clips with x and y array
+	1. out to in condition
+	2. out to out condition
+	3. in to out condition
+	4. in to in condition
--- Sutherland Hodgeman/SH.cpp
+++ Sutherland Hodgeman/SH.cpp
@@ -2,15 +2,23 @@
 #include<graphics.h>
 #include<math.h>
 
+// LHS Clipped Coordinates
 int LeftResultX[30];
 int LeftResultY[30];
+
+// Top Clipped Coordinates
 int TopResultX[30];
 int TopResultY[30];
+
+// RHS Clipped Coordinates
 int RightResultX[30];
 int RightResultY[30];
+
+// Bottom Clipped Coordinates
 int BottomResultX[30];
 int BottomResultY[30];
 
+// Boundary
 int xmin=20,ymin=40,xmax=100,ymax=100;
 
 int LeftClip(int x[],int y[],int n)
@@ -21,30 +29,43 @@ int LeftClip(int x[],int y[],int n)
         j=(i+1)%(n);
         if(x[i]<xmin && x[j]>=xmin) //out to in condition
         {
+            // x will be same
             LeftResultX[s]=xmin;
+
+            /*
+                (ymin - y1)/(y2 - y1) = (xmin - x1)/(x2- x1)
+            */
             LeftResultY[s]=y[i]+(xmin-x[i])*(y[j]-y[i])/(x[j]-x[i]);
+
             s++;
         }
         else if(x[i]<xmin && x[j]<xmin) //out to out condition
         {
+            // Don't add this to coordinate array
             continue;
         }
         else if(x[i]>=xmin && x[j]<xmin) //in to out condition
         {
+            // add the first coordinate as it is already in
             LeftResultX[s]=x[i];
             LeftResultY[s]=y[i];
             s++;
+
+            // add the next coordinate with ymin
             LeftResultX[s]=xmin;
             LeftResultY[s]=y[j]+(xmin-x[j])*(y[j]-y[i])/(x[j]-x[i]);
             s++;
         }
         else if(x[i]>=xmin && x[j]>=xmin) //in to in condition
         {
+            // this is accepted
             LeftResultX[s]=x[i];
             LeftResultY[s]=y[i];
             s++;
         }
     }
+
+    // s is number of coordinates in left clipped array
     return s;
 }
 
@@ -168,28 +189,47 @@ int main()
     int gd = DETECT, gm;
     initgraph(&gd, &gm, "");
 
+
+    // Draw the shape
     int x[]={55,55,15,65,115,75,75};
     int y[]={5,15,15,215,15,15,5};
+
     int n=7,j;
     drawwindow();
+
+    // Draw the initial shape
     for(int i=0;i<n;i++)
     {
+        // if i reaches the end, wrap it up to first element of array
         j=(i+1)%n;
         line(x[i],y[i],x[j],y[j]);
     }
+
     getch();
+
     cleardevice();
+
     drawwindow();
+
+    // Call left clip with x and y array
     int n1=LeftClip(x,y,n);
+
+    // print the left clipped shape
     for(int i=0;i<n1;i++)
     {
         j=(i+1)%n1;
         line(LeftResultX[i],LeftResultY[i],LeftResultX[j],LeftResultY[j]);
     }
     getch();
+
     cleardevice();
+
     drawwindow();
+
+    // call top clip with left clipped x and y array
     int n2=TopClip(LeftResultX,LeftResultY,n1);
+
+    // print the top clipped new shape
     for(int i=0;i<n2;i++)
     {
         j=(i+1)%n2;
